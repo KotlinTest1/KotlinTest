532889534d12eefeb87d2405bf62afaa90639efc
bogda <bogdan.nelepa01@mail.ru>
2023-09-05 19:58:27 +0500

    KS-1-3 flight into space

---
+++ src/main/kotlin/lesson_1/lesson_1_task_3.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+
+        val year = 1961
+
+        var hour = 10
+
+        var minute = 55
+
+        print(message = hour)
+
+        print(message = minute)
+
+}
