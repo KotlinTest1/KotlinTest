c6c05ae6a0be5003f3416e2222301ccf20e2f17b
bogda <bogdan.nelepa01@mail.ru>
2023-09-05 18:32:34 +0500

    KS-1-2 number of employees

---
+++ src/main/kotlin/lesson_1_task_2.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+
+    var numberOfOrders: Int = 75
+
+    var notificationText: String = "Спасибо за покупку, сообщение о покупке придет вам на почту!"
+
+    var numberOfEmployees = 2000
+
+    //println(numberOfEmployees)
+
+    numberOfEmployees = 1999
+
+    println(numberOfEmployees)
+
+}
