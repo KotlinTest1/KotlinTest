f787d613a6c42317f72d0fa910e50dd0fb06044e
bogda <bogdan.nelepa01@mail.ru>
2023-09-05 14:15:13 +0500

    KS-1-1 application screen logic for an online store

---
+++ src/main/kotlin/lesson_1_task_1.kt
@@ -0,0 +1,9 @@
+class lesson_1_task_1 {
+
+            var numberOfOrders: Int = 75
+
+            var notificationText: String = "Спасибо за покупку, сообщение о покупке придет вам на почту!"
+
+            println(numberOfOrders)
+            println(notificationText)
+        }
